4f0d524487ff5c8be1ac5a9aa62160bb51046ace
polinux87 <alessandro.poletti@gmail.com>
2017-07-24 18:10:22 +0200

    push

--- js/rolando.js
+++ js/rolando.js
@@ -137,7 +137,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("myCarousel");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
@@ -149,4 +149,33 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
+}
+
+//OTHER SLIDESHOW
+
+var slideIndex2 = 1;
+showDivs(slideIndex2);
+
+function plusDivs(n) {
+  showDivs(slideIndex2 += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex2 = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " w3-opacity-off";
 }
